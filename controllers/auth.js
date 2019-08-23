1f63bc014476ee48e275ac115a8d53714833bd09
Cliff Conolly <cliffconolly@protonmail.com>
2019-08-23 14:44:35 +0100

    one more casing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const User = require('../models/user')
+const User = require('../models/User')
 const Trip = require('../models/trip')
 const jwt = require('jsonwebtoken')
 const { secret } = require('../config/env')
